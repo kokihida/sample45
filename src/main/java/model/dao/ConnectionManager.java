e42e4f151a802d32d830a58ad870fff9140a598c
koki <suzumeiwa@gmail.com>
2024-01-29 11:51:44 +0900

    firstcommit

---
+++ src/main/java/model/dao/ConnectionManager.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	private static String URL = "jdbc:mysql://localhost:3306/itemdb";
+
+	/* データベースのユーザとパスワード */
+	private static String USER = "root";
+	private static String PASSWORD = "root";
+
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+
+	}
+}
